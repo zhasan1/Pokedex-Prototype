946db34961febeeb81c8fa91e209664c5e858737
zhasan1 <zubairhasan90@gmail.com>
2022-04-10 12:25:18 -0600

    added index, js and styles

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+/*JS code is always executed top to bottom, line by line.
+
+  alert('Hello World'); creates pop-up
+  console.log(); writes things into the console
+  document.write(); writes something on document*/
+
+alert('Hello World');
+
+
+let myName = 'Zubair';
+document.write(myName);
+
+let favoriteFood = 'Burger';
+document.write(favoriteFood);
