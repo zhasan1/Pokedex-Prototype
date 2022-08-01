732ecb8b210230d2ca981922976ab5d284a7497c
zhasan1 <zubairhasan90@gmail.com>
2022-08-01 08:36:05 -0600

    for to for each
    change for to for each

--- js/scripts.js
+++ js/scripts.js
@@ -54,7 +54,27 @@ let mixedArrays = [
 ];
 document.write(mixedArrays);*/
 
-alert("Welcome To The Pokedex!");
+//alert("Welcome To The Pokedex!");
+/*let pokemonRepository = (function (){
+  let pokemonList = [
+    {name: "Onyx", type: "Rock", height: 8.8},
+    {name: "Weedle", type: ["Bug", "Grass"] , height: 2}, 
+    {name: "Squirtle", type: "Water", height: 5},
+    {name: "Raidon", type: ["Rock", "Ground"] , height: 7.6}
+  ]; 
+  function getAll (){
+    return pokemonList;
+  }
+  function add(pokemon){
+    pokemon.List.push(pokemon);
+  }
+  return {
+    getAll: getAll,
+    add:add
+  };
+})();
+
+document.write(pokemonRepository.getAll());*/
 
 let pokemonList = [
   {name: "Onyx", type: "Rock", height: 8.8},
@@ -63,7 +83,7 @@ let pokemonList = [
   {name: "Raidon", type: ["Rock", "Ground"] , height: 7.6}
 ]; 
 
-for (let i = 0; i < pokemonList.length; i++){
+/*for (let i = 0; i < pokemonList.length; i++){
    if (pokemonList[i].type === ["Rock"||"Ground"]||["Rock"&&"Ground"] && pokemonList[i].height > 7){
      console.log(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "- Wow that is BIG!");
      document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "- Wow that is BIG!");
@@ -75,5 +95,46 @@ for (let i = 0; i < pokemonList.length; i++){
      document.write(pokemonList[i].name + "(height: " + pokemonList[i].height + ")" + "- that is small!");
    }
  }
- 
- 
+}*/
+
+function getName(pokemon){
+  return "Name: " + pokemon.name + "!";
+}
+
+
+function getHeight(pokemon){
+  if (pokemon.height > 0  && pokemon.height <5){
+    return "Height: " + pokemon.height + "- that is small!"  + "<br>";
+  }else if (pokemon.height === 5 && pokemon.height < 7){
+    return "Height: " + pokemon.height + "- that is average "  + "<br>";
+  }else if (pokemon.height > 7){
+    return "Height: " + pokemon.height + "- WOW! that is BIG"  + "<br>";
+  }else
+    return "Undefined";
+}
+
+function getType(pokemon) {
+ if (typeof(pokemon.type) === 'string') {
+    return "Type: "+pokemon.type;
+  }
+  else {
+    let str = '';
+    for (let i = 0; i < pokemon.type.length; i++){
+      str += " Types: " + pokemon.type[i] + ", "
+    }
+   return str
+  }
+  
+}
+
+function getList(pokemon) {
+  let str = '';
+  for (let i = 0; i < pokemon.length; i++){
+    str +=  getName(pokemon[i]) +  ", " +  getType(pokemon[i]) + ", " + getHeight(pokemon[i])
+  }
+  return str;
+}
+
+document.write(getList(pokemonList))
+
+
